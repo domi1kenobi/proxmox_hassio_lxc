69a4b04963711d6d7fc5cbf81741d6d63f1ccd75
domi1kenobi <domi1kenobi@gmail.com>
2023-08-09 02:22:15 +0200

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -52,8 +52,8 @@ pushd $TEMP_DIR >/dev/null
 
 # Create LXC
 export CTID=$(pvesh get /cluster/nextid)
-export PCT_OSTYPE=debian
-export PCT_OSVERSION=12
+export PCT_OSTYPE=ubuntu
+export PCT_OSVERSION=23.04
 export PCT_DISK_SIZE=4
 export PCT_OPTIONS="
   -cmode shell
